289b5cb8bcf441798c3904623556b074cb5303a2
Lucas Eckels <eckels@google.com>
2012-10-03 13:25:10 -0700

    Update MacBuild.sh to run on Xcode 3.2.6 on 10.7+
    Change-Id: I3bcfb972910dd16d9d8f2110dffb4bb7aaa947ca

--- MacBuild.sh
+++ MacBuild.sh
@@ -11,12 +11,13 @@ fi
 rm -rf build install
 mkdir -p build/ppc build/i386 install
 cd build/ppc
-../../configure --disable-ldap --with-ssl=`pwd`/../../../openssl --enable-static --disable-shared --target=powerpc-apple CFLAGS="-mmacosx-version-min=10.5 -arch ppc" CXXFLAGS="-mmacosx-version-min=10.5 -arch ppc" LDFLAGS="-mmacosx-version-min=10.5 -arch ppc" --prefix=`pwd`/../../install/ppc
-make
+# Use --host=ppc to keep configure from trying to run a generated ppc file, which will fail on 10.7+.
+../../configure --disable-ldap --with-ssl=`pwd`/../../../openssl --enable-static --disable-shared --target=powerpc-apple CXX=$DEVELOPER_BIN_DIR/g++ CC=$DEVELOPER_BIN_DIR/gcc CFLAGS="-mmacosx-version-min=10.5 -arch ppc -isysroot $SDKROOT" CXXFLAGS="-mmacosx-version-min=10.5 -arch ppc -isysroot $SDKROOT" LDFLAGS="-mmacosx-version-min=10.5 -arch ppc -isysroot $SDKROOT" --prefix=`pwd`/../../install/ppc --host=ppc --with-random=/dev/urandom
+make -j16
 make install
 cd ../i386
-../../configure --disable-ldap --with-ssl=`pwd`/../../../openssl --enable-static --disable-shared --target=i386-apple CFLAGS="-mmacosx-version-min=10.5 -arch i386" CXXFLAGS="-mmacosx-version-min=10.5 -arch i386" LDFLAGS="-mmacosx-version-min=10.5 -arch i386" --prefix=`pwd`/../../install/i386
-make
+../../configure --disable-ldap --with-ssl=`pwd`/../../../openssl --enable-static --disable-shared --target=i386-apple CXX=$DEVELOPER_BIN_DIR/g++ CC=$DEVELOPER_BIN_DIR/gcc CFLAGS="-mmacosx-version-min=10.5 -arch i386 -isysroot $SDKROOT" CXXFLAGS="-mmacosx-version-min=10.5 -arch i386 -isysroot $SDKROOT" LDFLAGS="-mmacosx-version-min=10.5 -arch i386 -isysroot $SDKROOT" --prefix=`pwd`/../../install/i386
+make -j16
 make install
 cd ../..
 
